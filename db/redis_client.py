7583c1099515ab1c1e2a4bbadc9876d7823b2598
WuLihua <1286045531@qq.com>
2018-03-19 22:43:33 +0800

    Feat:新增redis-client    -新增redis-client

--- db/redis_client.py
+++ db/redis_client.py
@@ -1,3 +1,28 @@
 # -*- coding: utf-8 -*-
 # @author Wu Lihua
 # @email maikekechn@gmail.com
+
+import redis
+from config import setup
+
+# redis连接
+def redis_connect():
+    redis_config = setup.get_sections(setup.redis_config_file, 'redis')
+    if not redis_config:
+        return False
+
+    try:
+        redis_conn = redis.StrictRedis(host=redis_config['hostname'], port=redis_config['port'])
+    except ConnectionAbortedError:
+        return False
+    else:
+        return redis_conn
+
+def set(key, value):
+    pass
+
+def get(key):
+    pass
+
+if __name__ == '__main__':
+    redis_connect()
