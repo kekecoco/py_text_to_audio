ffbf22011cf4b2b82b2abb1041aa5122e22a4891
wu.lihua <wu.lihua@immomo.com>
2018-03-19 16:47:00 +0800

    Feat: 配置文件模块  -配置文件模块

--- config/setup.py
+++ config/setup.py
@@ -2,9 +2,10 @@
 # @author Wu Lihua
 # @email maikekechn@gmail.com
 import configparser
+import os
 
 # 应用配置
-application_path = '/Users/deepin/py_text_to_audio'
+application_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 config_path = application_path + '/config'
 mysql_config_file = config_path + '/mysql.conf'
 redis_config_file = config_path + '/redis.conf'
@@ -13,14 +14,15 @@ error_log_path = application_path + '/runtime/error'
 
 
 # 配置文件初始化
-def configInit(config_file_name):
-    config_file = config_path + '/' + config_file_name
+def configInit(config_file):
     config_parser = configparser.ConfigParser()
     config_parser.read(config_file, 'utf-8')
     return config_parser
 
 # 返回可用的section列表
 def get_sections_title(config_file_name):
+    if not config_file_name:
+        return False
     config_parser = configInit(config_file_name)
     return config_parser.sections()
 
@@ -35,7 +37,12 @@ def get_sections(config_file_name, section):
     if not config_parser.has_section(section):
         return False
 
-    return config_parser.options(section)
+    sections = {}
+    sections_keys = config_parser.options(section)
+    for key in sections_keys:
+        sections[key] = config_parser.get(section, key)
+
+    return sections
 
 # 返回指定section中的某个值
 def get_option(config_file_name, section, option):
@@ -55,4 +62,5 @@ def get_option(config_file_name, section, option):
     return config_parser.get(section, option)
 
 if __name__ == '__main__':
-    pass
+    res = get_sections(mysql_config_file, 'mysql')
+    print(res['hostname'])
